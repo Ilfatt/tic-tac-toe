7e93a84876044c6dc44c092412d514aa9a13831f
ilfat <salavatyllinilfat@mail.ru>
2024-01-26 16:22:40 +0300

    правки

--- TicTacToeBackend/Api/Mappings/MappingProfile.cs
+++ TicTacToeBackend/Api/Mappings/MappingProfile.cs
@@ -18,8 +18,6 @@ public class MappingProfile : Profile
 
 		CreateMap<UserLoginRequest, UserLoginQuery>();
 		CreateMapFromResult<UserLoginResult, UserLoginResponse>();
-		
-		CreateMapFromResult<GetUserDataResult, GetUserDataResponse>();
 	}
 
 	private void CreateMapFromResult<TSource, TDestination>()
