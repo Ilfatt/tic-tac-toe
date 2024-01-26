76cfd3977285c70c6a11abb115ca9f278bdd4214
Marsel <arknahon@gmail.com>
2024-01-26 12:58:43 +0300

    Развертка фронта в докере

--- TicTacToeFrontend/vite.config.ts
+++ TicTacToeFrontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ tsDecorators: true })],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 })
